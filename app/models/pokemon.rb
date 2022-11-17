9dc4a342ed8c65c3a1a15ea96006b01c63fe8ef2
GabrielBentoG <gabrielbentog0@gmail.com>
2022-11-17 15:35:10 -0300

    Refactor in model Pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
     has_many :pokemon_types
     has_many :types, through: :pokemon_types
-    validates :name, presence: true
+    validates :name, :n_dex, presence: true
 end
